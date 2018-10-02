69732ca725f2e07cbbf3c61c2529bac30458379c
heriwahyudianto <heriwahyudianto@gmail.com>
2018-10-02 16:41:23 +0700

    script queue on head

--- inc/twenty20-shortcode.php
+++ inc/twenty20-shortcode.php
@@ -71,7 +71,8 @@ if(!empty($img1) && !empty($img2)){
   $output .= '<div class="twentytwenty-container '. $t20ID.' '.$yesHover.'"' . $isVertical . '>';
   $output .= '<img src="'. wp_get_attachment_url( $img1 ) .'"'.$img1_alt.' />';
   $output .= '<img src="'. wp_get_attachment_url( $img2 ) .'"'.$img2_alt.' />';
-  $output .= '</div><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>';
+  $output .= '</div><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="'.plugins_url().'/assets/js/jquery.twenty20.js"></script>';
   $output .= '<script>jQuery(document).ready(function($){';
   if($direction == "vertical"){
     $output .= '$(".twentytwenty-container.'.$t20ID.'[data-orientation=\'vertical\']").twentytwenty({default_offset_pct: ' . $offset . $data_vertical . $isHover . '});';
